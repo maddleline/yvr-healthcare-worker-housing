916c95afceea95ce6b66dd50465e3871d22bfd1b
Madeline Petersen <madeline.petersen@ibm.com>
2020-04-02 12:06:42 -0700

    Make site responsive

--- src/App.js
+++ src/App.js
@@ -24,30 +24,29 @@ function App() {
               <a href="#property-owners">For property owners</a>
             </div>
             <div className="bx--col-sm-0 bx--col-md-0 bx--col-lg-2">
-              <a href="#about">About</a>
+              <a href="#about">FAQs</a>
             </div>
           </div>
         </div>
 
-        <div className="bx--grid introduction bx--no-gutter">
-          <div className="bx--row section section__introduction">
-            <div className="bx--col-md-4 bx--col-lg-5 left">
+        <div className="introduction-bx--grid introduction">
+          <div className="introduction-bx--row section section__introduction">
+            <div className="left">
               <h1>Help keep healthcare workers' families safe</h1>
-              <p style={{ maxWidth: "360px" }}>
+              <p>
                 This a volunteer project designed to connect property owners
                 with healthcare workers that are looking for short-term housing
                 during the Covid-19 outbreak in Vancouver.
               </p>
             </div>
-            <div className="bx--offset-lg-1 left--gutter" />
-            <div className="bx--col-md-4 bx--col-lg-6 right border light-purple">
-              <div className="card container">
+            <div className="right">
+              <div className="card" style={{ borderBottom: "1px solid black" }}>
                 <h4>Healthcare workers</h4>
                 <h2>Looking for alternative housing?</h2>
                 <HealthWorkerModalWrapper />
               </div>
               <div className="divider black" />
-              <div className="card container">
+              <div className="card">
                 <h4>Property owners</h4>
                 <h2>Have an available furnished suite?</h2>
                 <PropertyOwnerLink />
@@ -58,7 +57,7 @@ function App() {
 
         <div className="bx--grid" id="healthcare-workers">
           <div className="bx--row section">
-            <div className="bx--col-md-4 bx--col-lg-5 left">
+            <div className="bx--col-md-4 bx--col-lg-5">
               <h3>Healthcare Workers</h3>
               <h2>Find temporary alternate housing</h2>
               <p>
@@ -69,27 +68,24 @@ function App() {
               </p>
               <HealthWorkerModalWrapper />
             </div>
-            <div className="bx--offset-lg-1 left--gutter" />
-            <div className="bx--col-md-4 bx--col-lg-3 right">
-              <div className="eyebrow container">
-                <div className="container">
-                  <h4>
-                    In order to verify those requesting access to our listings,
-                    we ask for the following personal information:
-                  </h4>
-                  <ul>
-                    <li>Name</li>
-                    <li>Occupation</li>
-                    <li>Place of work</li>
-                    <li>Email address</li>
-                  </ul>
-                </div>
-                <div className="container">
-                  <p>
-                    For flexibility, we recommend that you connect with
-                    coworkers who you could rent a suite with.
-                  </p>
-                </div>
+            <div className="bx--offset-lg-1" />
+            <div className="bx--col-sm-2 bx--col-md-4 bx--col-lg-3">
+              <div className="eyebrow">
+                <h4>
+                  In order to verify those requesting access to our listings, we
+                  ask for the following personal information:
+                </h4>
+                <ul>
+                  <li>Name</li>
+                  <li>Occupation</li>
+                  <li>Place of work</li>
+                  <li>Email address</li>
+                </ul>
+                <br />
+                <p>
+                  For flexibility, we recommend that you connect with coworkers
+                  who you could rent a suite with.
+                </p>
               </div>
             </div>
           </div>
@@ -97,7 +93,7 @@ function App() {
 
         <div className="bx--grid light-purple" id="property-owners">
           <div className="bx--row section">
-            <div className="bx--col-md-4 bx--col-lg-5 left">
+            <div className="bx--col-md-4 bx--col-lg-5">
               <h3>Property owners</h3>
               <h2>House a healthcare worker</h2>
               <p>
@@ -117,28 +113,26 @@ function App() {
                 workers at a cost of $500 each per month.
               </p>
             </div>
-            <div className="bx--offset-lg-1 left--gutter" />
-            <div className="bx--col-md-4 bx--col-lg-3 right">
-              <div className="eyebrow container">
-                <div className="container">
-                  <h4>Ideal properties will have the following amenities:</h4>
-                  <ul>
-                    <li>Separate entry</li>
-                    <li>Wifi</li>
-                    <li>On-site laundry</li>
-                    <li>Kitchen or kitchenette</li>
-                  </ul>
-                </div>
+            <div className="bx--offset-lg-1" />
+            <div className="bx--col-sm-2 bx--col-md-4 bx--col-lg-3">
+              <div className="eyebrow">
+                <h4>Ideal properties will have the following amenities:</h4>
+                <ul>
+                  <li>Separate entry</li>
+                  <li>Wifi</li>
+                  <li>On-site laundry</li>
+                  <li>Kitchen or kitchenette</li>
+                </ul>
               </div>
             </div>
           </div>
           <div className="bx--row section" id="list-property">
-            <div className="bx--col-md-4 bx--col-lg-12">
-              <div className="divider grey" />
+            <div className="bx--col-md-8 bx--col-lg-12">
+              <div className="divider--grey" />
             </div>
           </div>
           <div className="bx--row section">
-            <div className="bx--col-md-4 bx--col-lg-5 left">
+            <div className="bx--col-md-4 bx--col-lg-5">
               <h4>How do I list my property?</h4>
               <p>
                 Fill in this Google Form with your pertinent property
@@ -147,34 +141,33 @@ function App() {
               </p>
               <PropertyOwnerLink />
             </div>
-            <div className="bx--offset-lg-1 left--gutter" />
-            <div className="bx--col-md-4 bx--col-lg-5 right">
-              <div className="container">
-                <h4>My listing has been filled, how do I pull it?</h4>
-                <ol>
-                  <li>Email vancouver@healthworkerhousing.ca</li>
-                  <li>Put "Listing Complete" in the email subject line.</li>
-                  <li>
-                    Put your name in the body of the email, and the name of the
-                    listing you'd like to remove.
-                    <br />
-                    <br />
-                    That's it! Thank you for helping to keep a healthcare
-                    worker's family safe.
-                  </li>
-                </ol>
-              </div>
+            <div className="bx--offset-lg-1" />
+            <div className="bx--col-sm-4 bx--col-md-0 spacer" />
+            <div className="bx--col-md-4 bx--col-lg-5">
+              <h4>My listing has been filled, how do I pull it?</h4>
+              <ol>
+                <li>Email vancouver@healthworkerhousing.ca</li>
+                <li>Put "Listing Complete" in the email subject line.</li>
+                <li>
+                  Put your name in the body of the email, and the name of the
+                  listing you'd like to remove.
+                  <br />
+                  <br />
+                  That's it! Thank you for helping to keep a healthcare worker's
+                  family safe.
+                </li>
+              </ol>
             </div>
           </div>
         </div>
 
         <div className="bx--grid dark-purple">
-          <div className="bx--row section">
-            <div className="bx--col-md-4 bx--offset-lg-1 bx--col-lg-2 left">
+          <div className="bx--row section section__thank-you">
+            <div className="bx--col-md-8 bx--offset-lg-1 bx--col-lg-2">
               <h2>Thank you!</h2>
             </div>
-            <div className="bx--col-md-4 bx--offset-lg-1 bx--col-lg-7 right">
-              <div className="thank-you-message container">
+            <div className="bx--col-md-8 bx--offset-lg-1 bx--col-lg-7">
+              <div className="thank-you-message">
                 <p>
                   To the property owners who made their property available:  You
                   are providing a critical service to our front-line healthcare
