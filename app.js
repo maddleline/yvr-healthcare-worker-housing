ce9c57283316ee95ebb473f4935ff4b49629b920
Madeline Petersen <madeline.petersen@ibm.com>
2020-04-11 10:32:08 -0700

    Configure express server

--- app.js
+++
@@ -1,6 +0,0 @@
-var sslRedirect = require("heroku-ssl-redirect");
-var express = require("express");
-var app = express();
-
-// enable ssl redirect
-app.use(sslRedirect());
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const sslRedirect = require("heroku-ssl-redirect");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// enable ssl redirect
+app.use(sslRedirect());
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+
+console.log("App is listening on port " + port);
