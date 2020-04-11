ca0fd14af93feb585fbb1ef1d1b7271a73b90b03
Madeline Petersen <madeline.petersen@ibm.com>
2020-04-10 21:11:57 -0700

    Add heroku-ssl-redirect

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
+var sslRedirect = require("heroku-ssl-redirect");
 var express = require("express");
 var app = express();
 
-if (process.env.NODE_ENV === "production") {
-  app.use((req, res, next) => {
-    if (req.header("x-forwarded-proto") !== "https")
-      res.redirect(`https://${req.header("host")}${req.url}`);
-    else next();
-  });
-}
+// enable ssl redirect
+app.use(sslRedirect());
