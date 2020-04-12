76163f0eeff682714e54ee49ed2aa26473970909
Madeline Petersen <madeline.petersen@ibm.com>
2020-04-11 20:56:53 -0700

    Add manual https redirect

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const path = require("path");
-const sslRedirect = require("heroku-ssl-redirect");
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
 // enable ssl redirect
-app.use(sslRedirect());
-
-app.get("/ping", function (req, res) {
-  return res.send("pong");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https")
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    else next();
+  });
+}
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "index.html"));
